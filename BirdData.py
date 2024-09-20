927dae51861658b6515b0d28dc7a405bd386e877
Desmond Ergina <desergina@gmail.com>
2024-09-20 15:43:40 -0700

    streamlit app complete

--- BirdData.py
+++ BirdData.py
@@ -2,16 +2,15 @@
 import numpy as np
 import pandas as pd
 import geopandas as gpd
-import contextily as cx
-import matplotlib.pyplot as plt
 
 class Data:
 
     #import DataFrame with parrot information 
     data = pd.read_csv('red_parrots_observations.csv', sep='\t', low_memory=False)[['day', 'month', 'year', 'locality', 'decimalLatitude', 'decimalLongitude']]
+    data = data.rename(columns={'decimalLatitude': 'latitude', 'decimalLongitude': 'longitude'})
 
     #convert to GeoDataFrame and set projection
-    obsv = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data.decimalLongitude,data.decimalLatitude))
+    obsv = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data.longitude,data.latitude))
     obsv = obsv.set_crs(epsg=4326)
 
     #import SF Neighborhood Map shape file and set projection
@@ -24,34 +23,22 @@ class Data:
     #reproject map to fit base map
     con_map = map.to_crs(epsg=3857)
 
-    #load basemap
-    basemap, basemap_extent = cx.bounds2img(*con_map.total_bounds, zoom=15, ll = False, source=cx.providers.OpenStreetMap.Mapnik)
-
     def __init__(self):
-        #create new DataFrame dropping irrelevant columns and duplicate values
-        self.sightings = Data.nh[['day', 'month', 'year', 'decimalLatitude', 'decimalLongitude', 'geometry', 'name']].drop_duplicates()
+        self.sightings = Data.nh[['day', 'month', 'year', 'latitude', 'longitude', 'geometry', 'name']].drop_duplicates()
     
     def most_freq_loc(self):
         """returns locations parrots are most sighted at and prints map with those coordinates"""
 
-        locs = self.sightings[['name', 'decimalLatitude', 'decimalLongitude']]
-        cts = locs.value_counts().head(5).index
-        head = pd.DataFrame(list(cts), columns=['name', 'decimalLatitude', 'decimalLongitude']).set_index('name')
-        geo_head = gpd.GeoDataFrame(head, geometry=gpd.points_from_xy(head.decimalLongitude,head.decimalLatitude)).set_crs(epsg=4326)
-        con_geo = geo_head.to_crs(epsg=3857)
-        f, ax1 = plt.subplots(1, figsize= (15,15))
-        ax1.set_title('Most Frequent Sightings')
-        plt.imshow(Data.basemap, extent=Data.basemap_extent)
-        con_geo.plot(ax=plt.gca(), marker = 'o', markersize=100, alpha=.75)
-        ax1.set_axis_off()
-        plt.axis()
-        plt.show()
-        return list(cts)
+        locs = self.sightings[['name', 'latitude', 'longitude']]
+        cts = locs.value_counts().head(5)
+        df = pd.DataFrame(cts).reset_index()
+        df = df.rename(columns={'name': 'Neighborhood', 'count': 'Sighting Count'})
+        return df
     
     def by_season(self, season: str):
         """returns locations parrots are most sighted at during a season passed as parameter as well as percentage of total sightings"""
 
-        locs = self.sightings[['name', 'decimalLatitude', 'decimalLongitude', 'month']]
+        locs = self.sightings[['name', 'latitude', 'longitude', 'month']]
 
         sea = {
             'winter':locs[(locs['month'] == 12) | (locs['month'] == 1) | (locs['month'] == 2)],
@@ -60,36 +47,28 @@ class Data:
             'fall': locs[(locs['month'] == 9) | (locs['month'] == 10) | (locs['month'] == 11)]
         }
 
-        s = sea[season.lower()]
-        n = s.shape[0]
-        return  s.drop('month', axis=1).value_counts().head(5).index, n, (n / self.sightings.shape[0]) * 100
+        recs_season = sea[season.lower()]
+        n = recs_season.shape[0]
+        df = pd.DataFrame(recs_season.drop('month', axis=1).value_counts().reset_index())
+        df = df.rename(columns={'name': 'Neighborhood', 'count': 'Sighting Count'})
+        return  df, n, ((n/self.sightings.shape[0]) * 100)
         
         
     def no_in_neighborhood(self, location: str):
         """returns number of sightings in a neighborhood passed as parameter"""
 
         locs = self.sightings.groupby('name')
-        return locs.get_group(location).shape[0]
+        return locs.get_group(location)
     
     def most_at_location(self, location: str):
         """returns location parrots are most sighted at in a neighborhood passed as parameter"""
 
-        locs = self.sightings[['name', 'decimalLatitude', 'decimalLongitude']]
+        locs = self.sightings[['name', 'latitude', 'longitude']]
         l = locs[locs['name'] == location]
-        most = l.value_counts().head(1).index[0]
-        h = pd.DataFrame(most).transpose()
-        h.columns = ['name', 'decimalLatitude', 'decimalLongitude']
-        h = h.set_index('name')
-        p = gpd.GeoDataFrame(h, geometry=gpd.points_from_xy(h.decimalLongitude,h.decimalLatitude)).set_crs(epsg=4326)
-        con_p = p.to_crs(epsg=3857)
-        f, ax1 = plt.subplots(1, figsize= (15,15))
-        ax1.set_title(f'Most Frequent Sightings at {location}')
-        plt.imshow(Data.basemap, extent=Data.basemap_extent)
-        con_p.plot(ax=plt.gca(), marker = 'o', markersize=100, alpha=.75)
-        ax1.set_axis_off()
-        plt.axis()
-        plt.show()
-        return most
+        most = l.value_counts()
+        df = pd.DataFrame(most).reset_index()
+        df = df.rename(columns={'name': 'Neighborhood', 'count': 'Sighting Count'})
+        return df.sort_values('Sighting Count', ascending=False)
     
     def print_neighborhoods(self):
         """returns a list of neighborhoods parrots have been sighted in"""
@@ -101,29 +80,11 @@ class Data:
         """returns percentage of total sightings that are reported in neighborhood passed as parameter"""
 
         total = self.sightings.shape[0]
-        found = self.no_in_neighborhood(location)
+        found = self.no_in_neighborhood(location).shape[0]
         pc = (found / total) * 100
         return pc
     
-    def neighborhood_distribution(self):
-        """returns heat map of parrot sightings based on neighborhood"""
-        
-        n = self.sightings['name'].value_counts().index
-        v = self.sightings['name'].value_counts().values
-        df = pd.DataFrame({'name': n, 'count': v})
-        conm = Data.con_map.copy()
-        m = conm.merge(df, how='outer').fillna(0)
-        f, ax1 = plt.subplots(1, figsize=(15,15))
-        ax1.set_title("Sightings per Neighborhood")
-        plt.imshow(Data.basemap, extent=Data.basemap_extent)
-        m.plot('count', ax=plt.gca(), cmap='Wistia', alpha=.5, legend=True, legend_kwds={'orientation':"horizontal"})
-        ax1.set_axis_off()
-        plt.axis(m.total_bounds[[0,2,1,3]])
-        plt.show()
-
-
-    
 if __name__ == '__main__':
     bd = Data()
-    print(bd.data)
-    print('adsf')
+    data = bd.by_season('spring')
+    print(data)
---
+++ sf_parrots_streamlit.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import geopandas as gpd
+import streamlit as st
+import BirdData
+import altair as alt
+from datetime import date
+
+st.set_page_config(layout='centered')
+
+#load data
+birds = BirdData.Data()
+data = birds.sightings
+
+#introduction and videos
+st.title('San Francisco Wild Parrot Data')
+st.divider()
+st.header('Introduction', divider=True)
+st.markdown("Hello! On this page, you can access and view data about the San Francisco Wild Parrots! The San Francisco Wild Parrots are known for their distinctive green bodies, red colored heads, and loud calls. First making appearances in the 1990s, Psittacara erythrogenys, also known as Cherry-headed conures, are not native to San Francisco and are believed to have originated from Ecuador and Peru [(foundsf.org)](https://www.foundsf.org/index.php?title=Parrots_on_Telegraph_Hill). We'll take a closer look at the sightings of these special parrots in San Francisco." )
+vid1, vid2 = st.columns(2)
+with vid1:
+    st.video('videos/IMG_0812.MOV')
+with vid2:
+    st.video('videos/IMG_0815.MOV')
+
+#map of all data
+st.header('Map of All Sightings', divider=True)
+st.markdown('The data used in this project tracks sightings of the parrots around San Francisco between the years 2000 and 2024, with a total of 11,133 unique sightings! The top five locations that the parrots have been spotted most frequently can also be shown.')
+#top 5 locations
+most_freq = st.checkbox('Show Top 5 Locations')
+if most_freq:
+    st.map(birds.most_freq_loc(), size=25)
+    st.dataframe(birds.most_freq_loc(), hide_index=True, use_container_width=True)
+else:
+    st.map(data)
+
+#map of sightings per neighborhood
+st.header('Sightings Per Neighborhood', divider=True)
+st.markdown("We can also find how many times the parrots have been sighted in a given neighborhood, as well as the location they are most seen in that neighborhood. They definitely seem to favor certain neighborhoods, like Aquatic Park and Pacific Heights. Check out how often they are spotted in other neighborhoods by selecting one from the list below!")
+
+neighborhood = st.selectbox('Pick a neighborhood:', birds.print_neighborhoods(), index=None, placeholder='Neighborhood Name')
+if neighborhood is not None:
+    #statistics
+    sight, perc = st.columns(2)
+    with sight:
+        st.metric('Sightings', birds.no_in_neighborhood(neighborhood).shape[0])
+    with perc:
+        st.metric('Percentage of All Sightings', f"{birds.percent_of_sightings(neighborhood):.2f}%")
+    most_in_neighborhood = birds.most_at_location(neighborhood)
+    #top coordinates
+    show_most_in_neighborhood = st.checkbox(f'Show Top Locations In {neighborhood}')
+    if show_most_in_neighborhood:
+        st.map(most_in_neighborhood.head(5), size=10)
+    else:
+        st.map(birds.no_in_neighborhood(neighborhood), size=10)
+    st.dataframe(most_in_neighborhood.drop('Neighborhood', axis=1), hide_index=True, use_container_width=True)
+
+#map and chart of sightings per season
+st.header('Sightings Per Season', divider=True)
+st.subheader('Season Breakdown')
+#dataframe of seasonal sightings
+season_df = pd.DataFrame({'Season':['Spring', 'Summer', 'Fall', 'Winter'], 
+                          'Sightings': [birds.by_season('spring')[1], birds.by_season('summer')[1], birds.by_season('fall')[1], birds.by_season('winter')[1]],
+                          'Percentage': [f"{birds.by_season('spring')[2]:.2f}%", f"{birds.by_season('summer')[2]:.2f}%", f"{birds.by_season('fall')[2]:.2f}%", f"{birds.by_season('winter')[2]:.2f}%"]})
+#pie chart of seasonal sightings
+season_pie = alt.Chart(season_df).mark_arc(innerRadius=75).encode(
+        theta=alt.Theta(field="Sightings", type="quantitative"),
+        color=alt.Color(field="Season", type="nominal"))
+#seasonal sightings columns
+season_df_col, season_chart_col = st.columns(2, vertical_alignment='center')
+season_df_col.dataframe(season_df.sort_values('Sightings', ascending=False), hide_index=True, use_container_width=True)
+season_chart_col.altair_chart(season_pie, use_container_width=True)
+st.markdown("As shown above, there are slight variations in how often the parrots are spotted based on time of year. For the purposes of this project, we'll look at how often they're seen per season. According to the data, the spring months are when the parrots are most sighted and winter is when they're spotted the least.")
+
+st.subheader('Mapping Season Sightings')
+st.markdown('We can also map the amount of sightings per season to better visualize the differences. Make sure to look at the table below the map to see how the neighborhoods change.')
+st.markdown("If you look closely, you'll notice that no matter the season, the parrots are always seen the most in the Aquatic Park neighborhood and the Pacific Heights neighborhood. However, there is a quite a bit variance with the other neighborhoods throughout the seasons. Pick a season below to check it out!")
+season_list = st.selectbox('Pick a Season to View Map:', options=('Spring', 'Summer', 'Fall', 'Winter'), index=None, placeholder='Select a season...')
+if season_list is not None:
+    season_sightings = birds.by_season(season_list)[0]
+    st.map(season_sightings)
+    season_neighborhoods = pd.DataFrame(season_sightings.drop(['latitude', 'longitude'], axis=1).groupby('Neighborhood').sum())
+    st.dataframe(season_neighborhoods.sort_values('Sighting Count', ascending=False), use_container_width=True)
+
+st.header('Amount of Sightings Over Time', divider=True)
+st.markdown("The data that's used for this project is pulled from several databases using research-grade observations. By looking at the graph below, it's clear that there's been in an upward trend of research-grade observations of the San Francisco parrots in recent years, especially during the years of the COVID-19 pandemic. Despite them being in San Francisco since the 1990's, not many observations of the parrots were recorded prior to 2010. Hopefully, interest in the parrots continues to grow!")
+#area chart
+date_df = pd.DataFrame(data).astype({'year':'str'})
+sightings_over_time = date_df.drop(['day','month', 'latitude', 'longitude', 'geometry'], axis=1).groupby('year').count()
+st.area_chart(sightings_over_time, x_label='Year', y_label='Amount of Sightings', color=(173, 214, 255, 0.68))
+
+st.markdown("The amount of sightings per year can found and mapped as well. If you go through the years, you can see that the sightings were often very few and scattered throughout the city in the early 2000s. As the amount of sightings has risen, they have been particularly concentrated around the Aquatic Park neighborhood and Pacific Heights neighborhood. Other notable concentrations of sightings are in Telegraph Hill, Corona Heights, the Financial District, the Presidio and Golden Gate Park")
+st.markdown('Select a year below to see a map of the sightings for that year!')
+year_sightings = st.selectbox('Pick a year:', options=sightings_over_time.index.to_list(), index=None, placeholder='Select year...')
+
+#map of sightings per year
+if year_sightings is not None:
+    sightings_by_year = date_df.groupby('year')
+    st.map(sightings_by_year.get_group(year_sightings), size=50)
+
+    in_year = pd.DataFrame(sightings_by_year.get_group(year_sightings).drop(['day','month', 'latitude', 'longitude', 'geometry'], axis=1))
+    st.dataframe(pd.DataFrame(in_year.value_counts()).
+                 reset_index()[['name','count']].
+                 rename(columns={'name': 'Neighborhood', 'count': 'Sighting Count'}),
+                 hide_index=True,
+                 use_container_width=True)
+
+st.header('Conclusion', divider=True)
+st.markdown("Thanks for taking the time to check out my project! This started as a way to help my partner find the local parrots because she had never seen them in her entire time living in the Bay Area. I figured that if I could find out where they spend most of their time, I could just bring her to them. What started as just a fun tool to show my partner something she's been looking for since she was a kid ended up being my first data visualization project. I hope you learned a little more about these birds than you did before!")
+
+st.subheader('Sources')
+st.write("GBIF.org (16 April 2024) GBIF Occurrence Download https://doi.org/10.15468/dl.ch87pw")
+st.write('https://www.foundsf.org/index.php?title=Parrots_on_Telegraph_Hill')
